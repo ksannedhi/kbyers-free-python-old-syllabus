f1eec21d7ba8553d881a0a069e4129eedde07074
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 15:31:23 +0530

    Update week2_part2.py

--- week2_part2.py
+++ week2_part2.py
@@ -1,3 +1,12 @@
+'''Create an IP address converter (dotted decimal to binary):
+
+    A. Prompt a user for an IP address in dotted decimal format.
+    B. Convert this IP address to binary and display the binary result on the screen (a binary string for each octet).
+
+    Example output:
+    first_octet    second_octet     third_octet    fourth_octet
+    0b1010         0b1011000        0b1010         0b10011'''
+
 ip_addr = input("Provide an IP address: ")
 ip_addr_list = ip_addr.split(".")
 
