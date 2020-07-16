2f1fbfb19156f3c7de3a93088eda21eb51a9cdf5
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 15:47:53 +0530

    Update week4_part3.py

--- week4_part3.py
+++ week4_part3.py
@@ -1,3 +1,11 @@
+'''Create a program that converts the following uptime strings to a time in seconds.
+uptime1 = ‘twb-sf-881 uptime is 6 weeks, 4 days, 2 hours, 25 minutes’
+uptime2 = ‘3750RJ uptime is 1 hour, 29 minutes’
+uptime3 = ‘CATS3560 uptime is 8 weeks, 4 days, 18 hours, 16 minutes’
+uptime4 = ‘rtr1 uptime is 5 years, 18 weeks, 8 hours, 23 minutes’
+
+For each of these strings store the uptime in a dictionary using the device name as the key.'''
+
 from pprint import pprint
 
 uptime1 = 'twb-sf-881 uptime is 6 weeks, 4 days, 2 hours, 25 minutes'
