f944c58be226f4f89f6c985423201687f1df7e60
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 15:39:43 +0530

    Update week3_part3-1.py

--- week3_part3-1.py
+++ week3_part3-1.py
@@ -1,3 +1,20 @@
+'''The last exercise of the week is to create an IP address checker that checks the validity of an IP address. Here are the instructions:
+
+[code lang=text]
+IV. Create a script that checks the validity of an IP address. The IP address should be supplied on the command line.
+A. Check that the IP address contains 4 octets.
+B. The first octet must be between 1 – 223.
+C. The first octet cannot be 127.
+D. The IP address cannot be in the 169.254.X.X address space.
+E. The last three octets must range between 0 – 255.
+
+For output, print the IP and whether it is valid or not.
+[/code]
+
+The IP address will be supplied through the command line. 
+Like we’ve done before we are going to check the number of arguments supplied and exit the script if the number of arguments is not two.
+We need to import sys so that we can use sys.argv.'''
+
 import sys
 
 ip_addr = input("Provide an IP address to validate: ")
